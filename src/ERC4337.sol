beae6ae5cb495237427c03571332c286015ce984
Rafa <86583080+RafaBlockDev@users.noreply.github.com>
2023-03-08 21:43:39 -0600

    contract: create ERC4337 contract

---
+++ src/ERC4337.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+/**
+ * @title Basic ERC4337
+ * @author Rafael Fuentes
+ * github: https://github.com/RafaBlockDev
+ */
+
+contract ERC4337 {
+
+}
