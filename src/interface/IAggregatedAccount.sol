3a5ef70fec3db2bba50b5b3512226d4641b8aea7
Rafa <86583080+RafaBlockDev@users.noreply.github.com>
2023-03-26 14:49:39 -0600

    init substrate blockchain

--- src/interface/IAggregatedAccount.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
-
-import "./IAccount.sol";
-
-interface IAggregatedAccount is IAccount {
-    function getAggregator() external view returns(address);
-}
