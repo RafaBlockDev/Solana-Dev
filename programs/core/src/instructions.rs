e21eece83b5058465d0e70891a8889049cdf5aaa
Rafa <86583080+RafaBlockDev@users.noreply.github.com>
2023-03-29 13:48:42 -0600

    Create instructions.rs

---
+++ programs/core/src/instructions.rs
@@ -0,0 +1,59 @@
+use ark_ed_on_bn254::Fq;
+
+#[allow(clippy::comparison_chain)]
+
+pub fn check_external_amount(
+    
+) -> Result<(u64, u64), ProgramError> {
+
+}
+
+pub fn token_transfer<'a, 'b>(
+    token_program: &'b AccountInfo<'a>
+) -> Result<(u64, u64), ProgramError> {
+    
+}
+
+#[allow(clippy::clone_double_ref)]
+
+pub fn create_and_try_initialize_tmp_storage_pda(
+
+) -> Result<(), ProgramError> {
+
+}
+
+pub fn check_tx_integrity_hash(
+
+) -> Result<(), ProgramError> {
+
+}
+
+pub fn check_and_insert_nullifier<'a, 'b>(
+
+) -> Result<u8, ProgramError> {
+
+}
+
+pub fn close_account(
+
+) -> Result<(), ProgramError> {
+
+}
+
+pub fn sol_transfer(
+
+) -> Result<(), ProgramError> {
+
+}
+
+pub fn create_and_check_pda<'a, 'b>(
+
+) -> Result<(), ProgramError> {
+
+}
+
+pub fn try_initialize_tmp_storage_pda(
+
+) -> Result<(), ProgramError> {
+    
+}
