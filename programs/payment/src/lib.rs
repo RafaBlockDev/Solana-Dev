54a05d146d60377ce0b0dacacc2232c0d026051c
Rafa <86583080+RafaBlockDev@users.noreply.github.com>
2023-03-29 13:55:35 -0600

    Create a new module called "maius_payment" in "lib.rs" file

--- programs/payment/src/lib.rs
+++ programs/payment/src/lib.rs
@@ -1,15 +1,14 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::entrypoint::ProgramResult;
+use chrono::{Datelike, NaiveDate, NaiveDateTime};
+
+pub mod schema;
+pub mod instructions;
+use anchor_lang::solana_program::native_token::LAMPORTS_PER_SOL;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
-pub mod core {
-    use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
-    }
+pub mod maius_payment {
+    
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
